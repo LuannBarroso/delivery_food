26f6115328663d1ef82d2d6e265cf3459538f154
LuannBarroso <luan23.lba@gmail.com>
2023-03-15 23:28:02 -0400

    feat: create container in login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,7 +8,18 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Column(
-        children: const [],
+        children: [
+          Container(
+            height: 190,
+            width: 200,
+            decoration: const BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(150),
+              ),
+            ),
+          ),
+        ],
       )),
     );
   }
